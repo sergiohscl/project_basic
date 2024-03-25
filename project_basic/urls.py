782b79a57663e885ceeb76fcd519828a61648c72
Sergio <sergiohscl2@gmail.com>
2024-03-25 17:25:25 -0300

    :sparkles: urls

--- project_basic/urls.py
+++ project_basic/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
     )),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
